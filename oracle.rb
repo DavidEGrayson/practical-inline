9c798f1a3a7071957fe37d04423b245778ab0a7c
David Grayson <davidegrayson@gmail.com>
2017-04-09 15:13:42 -0700

    Got 44900 tests to pass.

--- oracle.rb
+++ oracle.rb
@@ -146,7 +146,7 @@ module InliningOracle
       end
 
       if t.inline_prototype? && t.gnu_inline_prototype? && t.always_inline_prototype? && t.extern_prototype?
-        if t.inline_definition? && t.gnu_inline_definition? && t.always_inline_definition? && !t.extern_definition?
+        if t.gnu_inline_definition? && t.always_inline_definition? && !t.extern_definition?
           return true
         end
       end
