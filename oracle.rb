2072d86a7c8a1067b96ce370a566cb9ca5973a5e
David Grayson <davidegrayson@gmail.com>
2017-04-09 15:17:15 -0700

    Got 45320 test cases to pass.

--- oracle.rb
+++ oracle.rb
@@ -150,6 +150,12 @@ module InliningOracle
           return true
         end
       end
+
+      if t.inline_prototype? && t.gnu_inline_prototype? && t.always_inline_prototype? && t.extern_prototype?
+        if t.inline_definition? && !t.gnu_inline_definition? && t.always_inline_definition? && !t.extern_definition?
+          return true
+        end
+      end
     end
 
     if [:c89, :gnu89].include?(language)
